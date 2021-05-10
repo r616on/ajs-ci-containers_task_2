daa4f88bd8f6a1da66eebfa46465768f812690a6
Roman Prilepsky <r616on@gmail.com>
2021-05-10 21:06:49 +0300

    Done_1_1

--- src/js/ErrorRepository.js
+++ src/js/ErrorRepository.js
@@ -1,9 +1,19 @@
 export default class ErrorRepository {
   constructor() {
     this.map = new Map();
-    this.map.set(1, 'Ошибка чтения');
-    this.map.set(2, 'Ошибка записи');
-    this.map.set(3, 'Ошибка типа данных');
+    this.map.set(1, 'Ошибка записи');
+  }
+
+  add(number, text) {
+    if (typeof number === 'number' && typeof text === 'string') {
+      if (!this.map.get(number)) {
+        this.map.set(number, text);
+      } else {
+        throw new Error('Номер уже существует');
+      }
+    } else {
+      throw new Error('Неверные данные');
+    }
   }
 
   translate(code) {
--- src/js/__tests__/ErrorRepository.test.js
+++ src/js/__tests__/ErrorRepository.test.js
@@ -2,9 +2,24 @@ import ErrorRepository from '../ErrorRepository';
 
 test('test ErrorRepository translate  ', () => {
   const error = new ErrorRepository();
-  expect(error.translate(2)).toBe('Ошибка записи');
+  expect(error.translate(1)).toBe('Ошибка записи');
 });
 test('test ErrorRepository translate there is no data ', () => {
   const error = new ErrorRepository();
   expect(error.translate(100)).toBe('Unknown error');
+});
+
+test('test ErrorRepository add watch', () => {
+  const error = new ErrorRepository();
+  error.add(3, 'Ошибка данных');
+  expect(error.translate(3)).toBe('Ошибка данных');
+});
+test('test ErrorRepository wrong data', () => {
+  const error = new ErrorRepository();
+  expect(() => error.add('3', 'Ошибка данных')).toThrow('Неверные данные');
+});
+test('test ErrorRepository the error already exists', () => {
+  const error = new ErrorRepository();
+  error.add(3, 'Ошибка данных');
+  expect(() => error.add(3, 'Ошибка данных')).toThrow('Номер уже существует');
 });
